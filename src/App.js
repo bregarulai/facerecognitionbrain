40e2a0ba7c11be8b9debdea9a523b2b8d5e095bc
bregarulai <bregarulai@gmail.com>
2020-09-24 16:39:57 -0400

    Created Rank component and added React Particle library

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React from "react";
 import "./App.css";
+import Particles from "react-particles-js";
 import Navigation from "./components/navigation/Navigation";
 import Logo from "./components/logo/Logo";
 import ImageLinkForm from "./components/imageLinkForm/ImageLinkForm";
+import Rank from "./components/rank/Rank";
 
 function App() {
+  const particlesOptions = {
+    particles: {
+      number: {
+        value: 50,
+        density: {
+          enable: true,
+          value_area: 900,
+        },
+      },
+    },
+  };
   return (
     <div className="App">
+      <Particles className="particles" params={particlesOptions} />
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {/*<Logo />
       <ImageLinkForm />
